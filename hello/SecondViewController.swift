0a0770fc5a00bf28e8a3adcc317fb4348e81c7a7
hmk20000 <hmk20000@nate.com>
2016-07-20 19:38:30 +0900

    first-commit
    check

---
+++ hello/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  hello
+//
+//  Created by sungwoo on 2016. 7. 20..
+//  Copyright © 2016년 mingu. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBAction func dismiss(sender: AnyObject) {
+        
+    }
+}
--- hello/ViewController.swift
+++ hello/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var uiTitle: UILabel!
     override func viewDidLoad() {
+        let apiURI = NSURL(string: "http://ip.jsontest.com")
+        let apidata : NSData? = NSData(contentsOfURL: apiURI!)
+        NSLog("API Result=%@",NSString(data:apidata!,encoding: NSUTF8StringEncoding)!)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sayHello(sender: AnyObject) {
+        self.uiTitle.text = "Hello, World!"
+    }
 
+    @IBAction func moveNext(sender: AnyObject) {
+        if let uvc = self.storyboard?.instantiateViewControllerWithIdentifier("SecondVC"){
+            uvc.modalTransitionStyle = UIModalTransitionStyle.PartialCurl
+            self.presentViewController(uvc, animated: true, completion: nil)
+        }
+    }
 }
 
